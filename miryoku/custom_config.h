cc604431773c7d7d9b3df003f2432e3a9166c76c
agisga <11449372+agisga@users.noreply.github.com>
2022-06-15 23:36:35 -0400

    same as my miryoku zmk config, minus the combos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,49 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_LAYERS_FLIP
+
+//--- modified from miryoku/mapping/42/corne.h
+
+#define XXX &none
+
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp TAB    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLH \
+&kp LPAR   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp RPAR \
+&kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LSHFT \
+                     K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTINNERINDEX 7 8
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTPINKIE 8 9
+
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTPINKIE 26 27
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTINNERINDEX 27 28
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTINNERINDEX 31 32
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTPINKIE 32 33
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 37 38
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
+
+//--- modified from miryoku_layer_alternatives.h
+
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&hm LGUI A,        &hm LALT S,        &hm LCTRL D,       &hm LSHFT F,       &kp G,             &kp H,             &hm LSHFT J,       &hm LCTRL K,       &hm LALT L,        &hm LGUI SQT,      \
+&lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &hm RALT DOT,      &lt U_BUTTON SLASH,\
+U_NP,              U_NP,              &lt U_FUN DEL,     &lt U_NUM BSPC,    &lt U_SYM RET,     &lt U_NAV SPC,     &lt U_NAV SPC,     &lt U_MEDIA ESC,   U_NP,              U_NP
+
+#define MIRYOKU_LAYER_EXTRA \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
+&kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &lt U_FUN DEL,     &lt U_NUM BSPC,    &lt U_SYM RET,     &lt U_NAV SPC,     &lt U_NAV SPC,     &lt U_MEDIA ESC,   U_NP,              U_NP
