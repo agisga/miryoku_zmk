2af65d3e642cc94354ac7d6ef56764f747e6dfbf
agisga <11449372+agisga@users.noreply.github.com>
2022-06-16 20:52:00 -0400

    sticky shift

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,11 +2,23 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_NAV_INVERTEDT
 #define MIRYOKU_CLIPBOARD_WIN
-#define MIRYOKU_LAYERS_FLIP
 
 //--- modified from miryoku/mapping/42/corne.h
+
+/ {
+  behaviors {
+    // Note: quick-release fixes the issue of sticky shift producing multiple upper case letters (see https://github.com/zmkfirmware/zmk/issues/903#issuecomment-909209198).
+    skq: sticky_key_quick_release {
+      compatible = "zmk,behavior-sticky-key";
+      label = "STICKY_KEY_QUICK_RELEASE";
+      #binding-cells = <1>;
+      bindings = <&kp>;
+      release-after-ms = <1000>;
+      quick-release;
+    };
+  };
+};
 
 #define XXX &none
 
@@ -16,10 +28,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp TAB    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLH \
-&kp LPAR   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp RPAR \
-&kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LSHFT \
-                     K32  K33  K34       K35  K36  K37
+&kp TAB       K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &key_repeat \
+&kp CAPSLOCK  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp RET \
+&kp LSHFT     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RSHFT \
+                        K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -40,10 +52,10 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &hm LGUI A,        &hm LALT S,        &hm LCTRL D,       &hm LSHFT F,       &kp G,             &kp H,             &hm LSHFT J,       &hm LCTRL K,       &hm LALT L,        &hm LGUI SQT,      \
 &lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &hm RALT DOT,      &lt U_BUTTON SLASH,\
-U_NP,              U_NP,              &lt U_FUN DEL,     &lt U_NUM BSPC,    &lt U_SYM RET,     &lt U_NAV SPC,     &lt U_NAV SPC,     &lt U_MEDIA ESC,   U_NP,              U_NP
+U_NP,              U_NP,              &lt U_MEDIA DEL,   &lt U_NAV BSPC,    &skq LSHFT,        &lt U_SYM SPC,     &lt U_NUM SPC,     &lt U_FUN ESC,     U_NP,              U_NP
 
 #define MIRYOKU_LAYER_EXTRA \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &lt U_FUN DEL,     &lt U_NUM BSPC,    &lt U_SYM RET,     &lt U_NAV SPC,     &lt U_NAV SPC,     &lt U_MEDIA ESC,   U_NP,              U_NP
+U_NP,              U_NP,              &lt U_MEDIA DEL,   &lt U_NAV BSPC,    &skq LSHFT,        &lt U_SYM SPC,     &lt U_NUM SPC,     &lt U_FUN ESC,     U_NP,              U_NP
