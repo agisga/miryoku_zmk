bf4dbbeba075b11bdd404a4e025206c14476fea4
agisga <11449372+agisga@users.noreply.github.com>
2022-06-19 17:55:35 -0400

    mouse layer is a symbols layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -28,10 +28,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp TAB     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &key_repeat \
-&bt BT_CLR  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp RET \
-&skq LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &skq RSHFT \
-                      K32  K33  K34       K35  K36  K37
+&kp LBKT        K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp RBKT \
+&kp TAB         K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &key_repeat \
+&mt LSHFT LPAR  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &mt RSHFT RPAR \
+                          K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -64,5 +64,5 @@ U_NP,              U_NP,              &lt U_FUN DEL,     &lt U_NUM BSPC,    &skq
 #define MIRYOKU_LAYER_MOUSE \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &to U_BASE,        &to U_EXTRA,       &to U_TAP,         &bootloader,       \
 &kp COLON,         &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &to U_SYM,         &to U_MOUSE,       &kp RALT,          U_NA,              \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          &kp UNDER,         &kp MINUS,         &kp LESS_THAN,     &kp GREATER_THAN,  &kp QUESTION,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
