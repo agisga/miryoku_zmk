132e36b48fc456a007f0af56560963906a2dda53
agisga <11449372+agisga@users.noreply.github.com>
2022-07-10 18:01:51 -0400

    trying zmk macros

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -20,6 +20,7 @@
   };
 };
 
+
 #define XXX &none
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
@@ -62,12 +63,12 @@ U_NP,              U_NP,              &lt U_FUN DEL,     &lt U_NUM BSPC,    &skq
 
 #define MIRYOKU_LAYER_TAP \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&hm LGUI A,        &hm LALT S,        &hm LCTRL D,       &hm LSHFT F,       &kp G,             &kp H,             &hm LSHFT J,       &hm LCTRL K,       &hm LALT L,        &hm LGUI SQT,      \
+&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
 U_NP,              U_NP,              &lt U_FUN DEL,     &mo U_NUM,         &skq LSHFT,        &kp SPC,           &mo U_NAV,         &lt U_MEDIA ESC,   U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &to U_BASE,        &to U_EXTRA,       U_NA,              &bootloader,       \
-&kp COLON,         &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp PLUS,          &mo U_MOUSE,       &kp BSPC,          &kp BSPC,          &kp BSPC,          &kp BSPC,          \
+&kp COLON,         &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp PLUS,          &mo U_MOUSE,       &kp BSPC,          &bspc2_macro,      &bspc3_macro,      &bspc5_macro,          \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          &kp UNDER,         &kp MINUS,         &kp LESS_THAN,     &kp GREATER_THAN,  &kp QUESTION,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
